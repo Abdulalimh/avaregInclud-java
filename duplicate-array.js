dc7ee321cb70a013f9618967767c7406bbecc9fc
Abdulalimh <mdabdul.alimbdbd@gmail.com>
2024-02-14 22:48:40 +0600

    no duplicate array

---
+++ duplicate-array.js
@@ -0,0 +1,21 @@
+
+// removed duplicate array , duble lekha array theke bad deya...
+
+function noDuplicate (array){
+    const unique = [];
+    for( const item of array){
+        if(unique.includes(item) === false){
+            unique.push(item);
+        }
+    }
+    return unique;
+}
+
+const briyaniKhor = ['khaled','korim','rashed','khaled','kuddus','korim'];
+const arrayNumbers = [22,55,23,20,12,22,55,20];
+
+const uniqueArray = noDuplicate(briyaniKhor);
+const uniqueNumbers = noDuplicate(arrayNumbers);
+console.log(uniqueArray);
+console.log(uniqueNumbers);
+
