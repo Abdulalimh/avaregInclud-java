498fa070dd9b29f840e543642a51532686fa154c
Abdulalimh <mdabdul.alimbdbd@gmail.com>
2024-03-24 14:56:25 +0600

    date and Time

--- convert.js
+++ convert.js
@@ -8,5 +8,31 @@ function heightWegth (inch){
     const result = feetNumber + ' ft. ' + feetRimaning + ' inch.';
     return result;
 }
+
+function mailToKilo (mile){
+ const kilo = mile * 1.60934 ;
+ const kiloNumber = parseInt(kilo);
+ const meter = mile % 1.60934 ;
+
+ const metersize = meter * 1000 ;
+ const meterNumber = parseInt(metersize);
+ const meters = meter % 1000 ;
+
+ const milisize = meter * 1000000 ;
+ const miliNumber = parseInt(milisize);
+ const mili = meters % 1000000 ;
+ const result = kiloNumber + ' Km ' + meterNumber + ' Meter ' + miliNumber + ' mm ' + mili ;
+ return result ;
+
+}
+
+
+ const toDay = new Date( '2024/11/13');
+ 
+
+
 const myHeight = heightWegth(75);
+const miles = mailToKilo(48);
 console.log(myHeight);
+console.log(miles);
+console.log(toDay.toLocaleString('en-GB'));
---
+++ dateTime.js
@@ -0,0 +1,4 @@
+const toDay = new Date( '2024/11/13');
+
+
+console.log(toDay.toLocaleString('en-GB'));
