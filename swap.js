8830171f7a8c461a3505b7ef706e49c4576d2dc1
Abdulalimh <mdabdul.alimbdbd@gmail.com>
2024-03-25 00:21:42 +0600

    man poriborton

---
+++ swap.js
@@ -0,0 +1,13 @@
+// eta dara ekta jinish ke onno jinisher sathe jayga poriborton kore
+
+let a = 5;
+let b = 8;
+
+console.log(a,b);
+
+// ekhane je kono ekta manke age alada kore onno ekta temp ba kisu ektar vitore rakhte hobe pore seta k jar man rekhesi tar jaygay bosha te hobe.... 
+const  x = a ;
+a=b;
+b=x;
+
+console.log(a,b);
